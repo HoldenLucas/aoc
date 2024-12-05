a94cfd6c94063488b32d65297a159a9f1cb18024
Holden Lucas <8461253+HoldenLucas@users.noreply.github.com>
2024-12-05 10:14:58 -0500

    3-25 templates

---
+++ 2024/clojure/src/aoc2024/day06.clj
@@ -0,0 +1,17 @@
+(ns aoc2024.day06
+  (:require [clojure.string :as str]
+            [hashp.core]))
+
+(defn parse-input
+  [input]
+  input)
+
+(defn part1
+  [input]
+  (->> input
+       parse-input))
+
+(defn part2
+  [input]
+  (->> input
+       parse-input))
