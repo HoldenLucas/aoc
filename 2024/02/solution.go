bc732c613335481b241826cd661515776983777e
Holden Lucas <8461253+HoldenLucas@users.noreply.github.com>
2025-10-07 12:04:06 -0400

    day 2 (init)

---
+++ 2024/02/solution.go
@@ -0,0 +1,17 @@
+package main
+
+func parse(path string) {}
+
+func part1() int {
+	return 0
+}
+
+func part2() int {
+	return 0
+}
+
+func main() {
+	print(part1())
+	print("\n")
+	print(part2())
+}
